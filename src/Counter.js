62843f1c3c3e9c416156bafb81bc8470a635c7cb
imadrgd20 <reguadiimad@gmail.com>
2024-08-23 17:37:24 +0100

    this is the first commit

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incByAmnt } from "./rdx_toolKit/features/counterSlice";
+
+export default function Counter(){
+    const count = useSelector(state=>state.counter.value);
+    const dispatch = useDispatch()
+
+    return(<>
+        
+    </>)
+}
